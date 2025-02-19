69eceb81028be8e7eebf9eb0a6d47d9ec24e35ab
Lenovo <Lenovo@DESKTOP-D921N34>
2025-02-19 16:28:02 +0200

    Add project files.

---
+++ Anagrams/Anagram.cs
@@ -0,0 +1,48 @@
+﻿namespace Anagrams;
+
+public class Anagram
+{
+    private readonly Dictionary<string, List<string>> _anagramDictionary = new();
+
+    public Anagram(List<string> dictionary)
+    {
+        var startTime = DateTime.Now;
+
+        foreach (var word in dictionary)
+        {
+            // Sort word then add to dictionary
+            // Ignore Casing in comparison
+          
+            var wordCaseInsensitive = word.ToLower().Trim();
+            var sortedWord = new string(wordCaseInsensitive.OrderBy(letter => letter).ToArray());
+
+            if (!_anagramDictionary.ContainsKey(sortedWord))
+            {
+                _anagramDictionary[sortedWord] = new List<string>();
+            }
+
+            _anagramDictionary[sortedWord].Add(wordCaseInsensitive);
+        }
+
+        var endTime = DateTime.Now;
+
+        var processingTime = endTime - startTime;
+        Console.WriteLine($"Performance testing: Processing time is {processingTime}");
+    }
+
+    public List<string> GetAnagram(string search)
+    {
+        var startTime = DateTime.Now;
+
+        var sortedWord = new string(search.ToLower().Trim().OrderBy(letter => letter).ToArray());
+        var anagramValues = _anagramDictionary.TryGetValue(sortedWord, out var anagrams)
+            ? anagrams
+            : new List<string>();
+
+        var endTime = DateTime.Now;
+        var retrieveTime = endTime - startTime;
+        Console.WriteLine($"Performance testing: Time for getting anagram list is {retrieveTime}");
+
+        return anagramValues;
+    }
+}
---
+++ Anagrams/Program.cs
@@ -0,0 +1,23 @@
+﻿using Anagrams;
+
+var dictionary = new List<string> { "Listen", "HEART", "Ear", "Silent", "Face", "Earth"," Farm ", "Are ", "Cafe", "Break", "Music",  "  baker", " Brake ", "dinosaur", "Door", "HATER",  "Angel", "Below", "Bowel", "angle", "angels", "elbow" };
+
+var anagram = new Anagram(dictionary);
+
+while (true)
+{
+    Console.WriteLine("Please Enter a word.");
+    var input = Console.ReadLine();
+
+    List<string> anagramList = anagram.GetAnagram(input ?? string.Empty);
+
+    if (anagramList.Count == 0)
+    {
+        Console.WriteLine($"Word {input} doesn't have an anagram match.");
+    }
+    else
+    {
+        Console.WriteLine($"Anagram list are: {string.Join(", ", anagramList)}");
+
+    }
+}
